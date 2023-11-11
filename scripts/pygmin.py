1219313777a0cd3df9a2b1c1fd27bbed3ac1b547
Ryan Castellucci <code@ryanc.org>
2023-11-11 09:16:31 +0000

    better pygment minifier

--- scripts/pygmin.py
+++ scripts/pygmin.py
@@ -4,6 +4,8 @@ from sys import argv, exit, stdin, stdout, stderr, version_info
 from functools import partial
 eprint = partial(print, file=stderr)
 
+import re
+
 import tinycss2
 from tinycss2.ast import *
 
@@ -20,18 +22,30 @@ def notwsp(token):
     return not isinstance(token, WhitespaceToken)
 
 def tokenstr(token, prev=None, next=None):
+    #eprint(token)
     s = NotAString()
-    if isinstance(token, HashToken):
-        s = '#' + token.value
-    elif isinstance(token, StringToken):
-        s = f'"{token.value}"'
-    elif isinstance(token, SquareBracketsBlock):
-        s  = '['
-        for sub in token.content:
-            s += tokenstr(sub)
-        s += ']'
-    else:
-        s = str(token.value)
+    try:
+        if isinstance(token, HashToken):
+            s = '#' + token.value
+        elif isinstance(token, StringToken):
+            s = f'"{token.value}"'
+        elif isinstance(token, FunctionBlock):
+            s = token.serialize()
+        elif isinstance(token, URLToken):
+            s = str(token.value)
+        elif isinstance(token, NumberToken):
+            n = token.value
+            i = int(n)
+            s = str(i if n == i else n)
+        elif isinstance(token, SquareBracketsBlock):
+            s  = '['
+            for sub in token.content:
+                s += tokenstr(sub)
+            s += ']'
+        else:
+            s = str(token.value)
+    except AttributeError as e:
+        eprint('oops', token, dir(token))
 
     return s
 
@@ -40,14 +54,38 @@ mapping = {}
 rules = tinycss2.parse_stylesheet(data)
 for rule in rules:
     if isinstance(rule, QualifiedRule):
-        selector = ''.join(map(tokenstr, filter(notwsp, rule.prelude)))
-        selector = selector.replace('.pgcss', '.')
-        properties = ''.join(map(str.strip, map(tokenstr, rule.content))).strip('; ')
-        for p in properties.split(';'):
-            if p == 'color:inherit': continue
-            selectors = mapping.setdefault(p, [])
-            selectors.append(selector)
+        selectors = ['']
+        for t in rule.prelude:
+            if isinstance(t, LiteralToken) and t.value == ',':
+                selectors.append('')
+            else:
+                selectors[-1] += tokenstr(t)
 
-for properties, selectors in mapping.items():
-    selector = ','.join(map(lambda s: s.replace('.', '.pgcss'), sorted(selectors)))
-    print(f'{selector}{{{properties}}}')
+        if selectors[-1] == '': selectors.pop()
+
+        #selector = selector.replace('.pgcss', '.')
+
+        properties = ['']
+        for t in rule.content:
+            if isinstance(t, LiteralToken) and t.value == ';':
+                properties.append('')
+            else:
+                properties[-1] += tokenstr(t)
+
+        for p in properties:
+            if p == 'color:inherit': continue
+            p = re.sub(r'^([a-z-]+:)\s*', r'\1', p.strip())
+            if p:
+                sel_list = mapping.setdefault(p, [])
+                for s in map(str.strip, selectors):
+                    if s: sel_list.append(s)
+
+    elif isinstance(rule, AtRule):
+        print(rule.serialize())
+
+for p, selectors in mapping.items():
+    for s in selectors:
+        print(f'{s} {{{p}}}')
+    #selector = ','.join(map(lambda s: s.replace('.', '.pgcss'), sorted(selectors)))
+    #selector = ', '.join(sorted(selectors))
+    #print(f'{selector} {{{properties}}}')
